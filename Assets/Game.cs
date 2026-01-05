6e55abf8b86ec632ebc357c6b00244515e9ebb4e
Bruger <phillipappel2007@gmail.com>
2026-01-05 19:17:29 +0100

    første push

---
+++ Assets/Game.cs
@@ -0,0 +1,76 @@
+﻿namespace Snake.Assets;
+using Raylib_cs;
+
+public class Game
+{
+    
+    private static Game? _instance;
+    public static Game Instance
+        => _instance ?? throw new InvalidOperationException("Game ikke initialiseret");
+  
+    
+    private readonly int _width;
+    private readonly int _height;
+    private readonly string _title;
+    
+    //offentlige properties så andre klasser kan bruge
+    private int Width => _width;
+    private int Height => _height;
+    
+    
+    public Game(int width, int height, string title)
+    {
+        if (_instance != null)
+        {
+            throw new InvalidOperationException("Game er allerede initialiseret");
+        }
+       
+        _instance = this;
+        _width = width;
+        _height = height;
+        _title = title;
+    }
+    
+    public void Run()
+    {
+        
+        Initalize();
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Update();
+            Draw();
+        }
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    
+    private void Initalize()
+    {
+       
+        Raylib.InitWindow(_width, _height, _title);
+        Raylib.SetTargetFPS(60);
+        
+    }
+
+    private void Draw()
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(color:Color.RayWhite);
+        
+        Raylib.EndDrawing();
+        
+    }
+
+    private void Cleanup()
+    {
+        
+        Raylib.CloseWindow();
+        
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using Snake.Assets;
+using colors = Raylib_cs.Color;
+
+class Program
+{
+    static void Main()
+    {
+        var game = new Game(600, 600, "Snake");
+        game.Run();
+     
+    }
+}
