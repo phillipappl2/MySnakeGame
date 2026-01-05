1044356ea9e591bfcec59a0ed1baa277f1832102
Bruger <phillipappel2007@gmail.com>
2026-01-05 21:20:55 +0100

    add

---
+++ Systems/UpdatebleInterfaces.cs
@@ -0,0 +1,13 @@
+﻿namespace Snake.Utils;
+
+public interface IUpdatable
+{
+    
+    void Update();
+    
+}
+
+public interface IDrawable
+{
+    void Draw();
+}
---
+++ Utils/Direction.cs
@@ -0,0 +1,6 @@
+﻿namespace Snake.Utils;
+
+public enum Direction
+{
+    Up, Down, Left, Right
+}
