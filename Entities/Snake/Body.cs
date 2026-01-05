00dda24e9eb544e79ede8391a4d79c0e827398fa
Bruger <phillipappel2007@gmail.com>
2026-01-05 19:57:03 +0100

    added

---
+++ Entities/Snake/Body.cs
@@ -0,0 +1,19 @@
+﻿namespace Snake.Entities.Snake;
+
+public class Body
+{
+
+    public Body(int x = 0, int y = 0)
+    {
+        X = x;
+        X = y;
+    }
+
+    public int X { get; set; }
+    
+
+    public int Y { get; set; }
+
+    public void SetPositon(int x, int y) => (X, Y) = (x, y);
+   
+}
---
+++ Entities/Snake/SnakeLogic.cs
@@ -0,0 +1,56 @@
+﻿namespace Snake.Entities.Snake;
+
+public class SnakeLogic
+{
+    
+    
+    private List<Body> _body;
+    private Body Head = new Body();
+
+    private int LastX;
+    private int LastY;
+    
+    public SnakeLogic(int posX, int posY)
+    {
+        _body = [];
+        _body.Add(Head);
+        
+        LastX = posX;
+        LastY = posY;
+
+    }
+
+    private void Update()
+    {
+        LastX = _body[^1].X; 
+        LastY = _body[^1].Y;
+        for (int i = _body.Count-1; i >= 1; i--)
+        {
+            _body[i].X = _body[i - 1].X;
+            _body[i].Y = _body[i - 1].Y;
+        }
+    }
+
+    private void Grow(Body body)
+    {
+        body.SetPositon(LastX, LastY);
+        _body.Add(body);
+        
+    }
+
+    private void Fill(Body body)
+    {
+        
+        
+    }
+    
+    private void Draw()
+    {
+        for (int i = 0; i < _body.Count; i++)
+        {
+            Fill(_body[i]);
+        }
+        
+    }
+    
+}
